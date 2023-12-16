8616f81d0fff064079c2bae1354ffbab63d34c8e
Denis Carlos de Farias <denis_carlos_farias@hotmail.com>
2023-12-16 11:48:12 -0300

    fith: Inclusao de serializacao e deserializacao

--- blockchain/block.go
+++ blockchain/block.go
@@ -3,6 +3,8 @@ package blockchain
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 )
 
 type Block struct {
@@ -32,4 +34,31 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	block.Hash = hash[:]
 
 	return block
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	Handler(err)
+
+	return result.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+	Handler(err)
+
+	return &block
+}
+
+func Handler(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
